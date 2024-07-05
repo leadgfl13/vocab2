7ca3dbe97772984aae7aacf94c4539731cc48cc1
leadgfl13 <leadgfl13@gmail.com>
2024-07-05 09:49:00 -0400

    Have dropdownn menu stay visible when items are hovered

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import "./style.css";
-
+//Glocal variables on main page
 let menu = document.getElementById("menubutton");
+let item1 = document.getElementById("dropdown1");
+let item2 = document.getElementById("dropdown2");
+let item3 = document.getElementById("dropdown3");
+let item4 = document.getElementById("dropdown4");
+//
+
+item1.addEventListener("click", () => {
+	alert("Item 1 was cicked");
+});
 
 menu.addEventListener("mouseover", () => {
 	let test = document.getElementById("dropdowncontainer");
