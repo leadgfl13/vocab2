afcba6543217d9984ecdfd97db05e22967e0d304
leadgfl13 <leadgfl13@gmail.com>
2024-07-10 20:47:12 -0400

    Add code to iffe to create dots based on how long the images array is and put them in the dots carousel div

--- src/modules/item1.js
+++ src/modules/item1.js
@@ -1,7 +1,7 @@
 let thing = ["I am thing1", "I am thing2"];
 let image1 = "url(../src/images/stardewvalley.jpg)";
 let image2 = "url(../src/images/dog.jpeg)";
-let image3 = "url(../src/images/halo.webp)";
+let image3 = "url(../src/images/greendragon.jpeg)";
 
 export function sayHello() {
 	alert(thing[1]);
@@ -19,13 +19,24 @@ export function menu3() {
 export default (function moveCarousel() {
 	let images = [image1, image2, image3];
 	let counter = 0;
+
+	//creates dots for carousel and adds them to the page
+	for (let i = 0; i < images.length; i++) {
+		let dot = document.createElement("div");
+		dot.setAttribute("class", "circle");
+		let carouseldots = document.getElementById("carouseldots");
+		carouseldots.append(dot);
+	}
+	//sets defaults image to 0
 	carouselcontainer.style.backgroundImage = images[counter];
+
+	//functions for left and right arrow, and changing counter
 
 	let leftarrow = document.getElementById("left");
 	let rightarrow = document.getElementById("right");
 	leftarrow.addEventListener("click", () => {
 		if (counter == 0) {
-			counter = images.length + 1;
+			counter = images.length - 1;
 		} else {
 			counter = counter - 1;
 		}
@@ -36,7 +47,7 @@ export default (function moveCarousel() {
 	});
 
 	rightarrow.addEventListener("click", () => {
-		if (counter == images.length + 1) {
+		if (counter == images.length - 1) {
 			counter = 0;
 		} else {
 			counter = counter + 1;
