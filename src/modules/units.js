4ed1a7361f411ca6bed07857107e160092cfa3aa
leadgfl13 <leadgfl13@gmail.com>
2024-07-24 18:06:48 -0400

    Add vocab for unit 2

--- src/modules/units.js
+++ src/modules/units.js
@@ -30,27 +30,40 @@ export let unit1_vocab = {
 };
 
 export let unit2_vocab = {
-	"unit 2 word":
-		"Definition 1 this is a test to see how making the definition bigger affects the div size",
-	"word 2": "DEFINITION 2",
-	"word 3": "DEFINITION 3",
-	"word 4": "DEFINITION 4",
-	"word 5": "Definition 5",
-	"Word 6": "definition 6",
-	"word 7": "definition 7",
-	"word 8": "defintion 8",
-	"word 9": "definition 9",
-	"word 10": "definition 10",
-	"word 11": "definition 11",
-	"word 12": "Definition 12",
-	"word 13": "Defintion 13",
-	"word 14": "definition 14",
-	"word 15": "definition 15",
-	"word 16": "Definition 16",
-	"word 17": "Defintion 17",
-	"word 18": "definition 18",
-	"word 19": "definition 19",
-	"word 20": "Definition 20",
+	Cell: "basic unit of life",
+	"Cell theory":
+		"All living things are made of cells, cells come from other cells",
+	"Cell membrane (Plasma membrane)":
+		"A thin flexible layer on the outside of cell made up of phospholipid bilayer(two layers of fats)",
+	Nucleus:
+		"large membrane enclosed structure that contains the genetic material(DNA) of eukaryotic cells.  Directs many cell activites, only found in Eukaryotic cells",
+	"Eukaryotic Cell":
+		"cells that have membrane bound organelles (structures enclosed in a protective layer that separate them from the rest of the cell)",
+	"Prokaryotic Cell": 'a "simple" cell with no organelles',
+	Cytoplasm: "the liquid inside of a cell",
+	Organelles:
+		'"little organs" inside of the cell that carry out specialized functions',
+	Vacuoles:
+		"organlle that is used for storage of water, proteins, carbohydrates, etc",
+	Lysosomes:
+		"organelles that are filled with enzymes to help break down materials, including other organlles",
+	Cytoskeleton:
+		"network of filaments that help give the cell structure (Includes microfilaments and microtubules)",
+	Centrioles:
+		"small structures located near the nucleus that help organize cell division",
+	Ribosomes: "structures responsible for creating proteins out of amino acids",
+	"Endpoplasmic Recticulum":
+		"assembles lipids(fats), proteins, and other materials that are exported from the cell",
+	"Golgi Apparatus":
+		"Modifies, sorts and packages proteins and other materials from the endoplasmic reticulum for storage in the cell or release outside the cell",
+	Mitochondria:
+		"Site of cellular respiration where oxygen and sugar are converted into cellular energy in the form of ATP",
+	Chloroplasts:
+		"Capture the energy of sunlight and convert it along with Carbon Dioxide into sugar (glucose) and oxygen",
+	"Cell wall":
+		"Rigid structure found in some cells on the outside of the cell membrane that provides support and protection",
+	"Selectively(Semi) Permeable":
+		"A membrane(covering) that allows some substances to cross, but not others.  In the case of the cell membrane it is based on size",
 };
 
 export let unit3_vocab = {
