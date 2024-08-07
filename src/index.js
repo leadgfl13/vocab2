8191e285e6e1ac1e24da2e45c51652e4921e883e
leadgfl13 <leadgfl13@gmail.com>
2024-08-06 21:30:54 -0400

    Update each word and definition function to use the counter object

--- src/index.js
+++ src/index.js
@@ -189,91 +189,131 @@ item8.addEventListener("mouseleave", () => {
 //event listener for selection of choice on unit 1
 word1.addEventListener("click", () => {
 	const [newkeys, newvalues] = moveCarousel(unit1_vocab, 0);
-
 	doDots(newkeys, newvalues);
-
 	keys = newkeys;
 	values = newvalues;
 	checkDots();
-
 	topright.innerHTML = "Scientific Method";
-	return counter, keys, values;
 });
 
 definition1.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit1_vocab, 1);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit1_vocab, 1);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Scientific Method";
 });
 word2.addEventListener("click", () => {
-	console.log("Hello");
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit2_vocab, 0);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit2_vocab, 0);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Life and Cells";
 });
 definition2.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit2_vocab, 1);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit2_vocab, 1);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Life and Cells";
 });
 word3.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit3_vocab, 0);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit3_vocab, 0);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Nutrition";
 });
 definition3.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit3_vocab, 1);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit3_vocab, 1);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Nutrition";
 });
 word4.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit4_vocab, 0);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit4_vocab, 0);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Homeostasis";
 });
 definition4.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit4_vocab, 1);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit4_vocab, 1);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Homeostasis";
 });
 word5.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit5_vocab, 0);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit5_vocab, 0);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Respiration/Photosynthesis";
 });
 definition5.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit5_vocab, 1);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit65_vocab, 1);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Respiration/Photosynthesis";
 });
 word6.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit6_vocab, 0);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit6_vocab, 0);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Genetics";
 });
 definition6.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit6_vocab, 1);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit6_vocab, 1);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Genetics";
 });
 word7.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit7_vocab, 0);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit7_vocab, 0);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Evolution";
 });
 definition7.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit7_vocab, 1);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit7_vocab, 1);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Evolution";
 });
 word8.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit8_vocab, 0);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit8_vocab, 0);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Ecology";
 });
 definition8.addEventListener("click", () => {
-	const [newcounter, newkeys, newvalues] = moveCarousel(unit8_vocab, 1);
-	(counter = newcounter), (keys = newkeys), (values = newvalues);
+	const [newkeys, newvalues] = moveCarousel(unit8_vocab, 1);
+	doDots(newkeys, newvalues);
+	keys = newkeys;
+	values = newvalues;
+	checkDots();
 	topright.innerHTML = "Ecology";
 });
 
